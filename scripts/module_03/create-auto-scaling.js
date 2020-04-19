c7afd754b1494b2030520f3d2fd8035f79b3f84d
Dien Luong <dysonlu@gmail.com>
2020-04-19 17:23:07 -0400

    New: implemented auto-scaling and load balancing

--- scripts/module_03/create-auto-scaling.js
+++ scripts/module_03/create-auto-scaling.js
@@ -1,23 +1,60 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-const autoScaling = new AWS.AutoScaling()
-const asgName = 'hamsterASG'
-const lcName = 'hamsterLC'
-const policyName = 'hamsterPolicy'
-const tgArn = '/* TODO: get target group ARN */'
+const autoScaling = new AWS.AutoScaling();
+const asgName = 'hamsterASG';
+const lcName = 'hamsterLC';
+const policyName = 'hamsterPolicy';
+const tgArn = 'arn:aws:elasticloadbalancing:us-east-1:206615147391:targetgroup/hamsterTG/5cf54b65ac2771d7';
 
 createAutoScalingGroup(asgName, lcName)
 .then(() => createASGPolicy(asgName, policyName))
-.then((data) => console.log(data))
+.then((data) => console.log(data));
 
 function createAutoScalingGroup (asgName, lcName) {
-  // TODO: Create an auto scaling group
+  const params = {
+    AutoScalingGroupName: asgName,
+    AvailabilityZones: [
+      'us-east-1c',
+      'us-east-1d'
+    ],
+    TargetGroupARNs: [
+      tgArn,
+    ],
+    LaunchConfigurationName: lcName,
+    MaxSize: 2,
+    MinSize: 1,
+  };
+
+  return new Promise((resolve, reject) => {
+    autoScaling.createAutoScalingGroup(params, (err, data) => {
+      if (err) { reject(err); return;}
+      else { resolve(data); return;}
+    });
+  });
 }
 
 function createASGPolicy (asgName, policyName) {
-  // TODO: Create an auto scaling group policy
+  const params = {
+    AdjustmentType: 'ChangeInCapacity',
+    AutoScalingGroupName: asgName,
+    PolicyName: policyName,
+    PolicyType: 'TargetTrackingScaling',
+    TargetTrackingConfiguration: {
+      TargetValue: 5,
+      PredefinedMetricSpecification: {
+        PredefinedMetricType: 'ASGAverageCPUUtilization',
+      }
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    autoScaling.putScalingPolicy(params, (err, data) => {
+      if (err) { reject(err); return; }
+      else { resolve(data); return; }
+    });
+  });
 }
--- scripts/module_03/create-load-balancer.js
+++ scripts/module_03/create-load-balancer.js
@@ -1,18 +1,20 @@
 // Imports
-const AWS = require('aws-sdk')
-const helpers = require('./helpers')
+const AWS = require('aws-sdk');
+const helpers = require('./helpers');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
 // TODO: Create a new ELBv2 object
-const sgName = 'hamsterELBSG'
-const tgName = 'hamsterTG'
-const elbName = 'hamsterELB'
-const vpcId = '/* TODO: Add your VPC Id */'
+const elbv2 = new AWS.ELBv2();
+const sgName = 'hamsterELBSG';
+const tgName = 'hamsterTG';
+const elbName = 'hamsterELB';
+const vpcId = 'vpc-20ffff5a';
 const subnets = [
-  /* TODO: Add two subnets */
-]
+  'subnet-0a931447',
+  'subnet-50f5050f',
+];
 
 helpers.createSecurityGroup(sgName, 80)
 .then((sgId) =>
@@ -22,15 +24,28 @@ helpers.createSecurityGroup(sgName, 80)
   ])
 )
 .then((results) => {
-  const tgArn = results[0].TargetGroups[0].TargetGroupArn
-  const lbArn = results[1].LoadBalancers[0].LoadBalancerArn
-  console.log('Target Group Name ARN:', tgArn)
+  const tgArn = results[0].TargetGroups[0].TargetGroupArn;
+  const lbArn = results[1].LoadBalancers[0].LoadBalancerArn;
+  console.log('Target Group Name ARN:', tgArn);
   return createListener(tgArn, lbArn)
 })
-.then((data) => console.log(data))
+.then((data) => console.log(data));
 
 function createLoadBalancer (lbName, sgId) {
-  // TODO: Create a load balancer
+  const params = {
+    Name: lbName,
+    Subnets: subnets,
+    SecurityGroups: [
+      sgId,
+    ]
+  };
+
+  return new Promise((resolve, reject) => {
+    elbv2.createLoadBalancer(params, (err, data) => {
+      if (err) { reject(err); return; }
+      else { resolve(data); return; }
+    });
+  });
 }
 
 function createTargetGroup (tgName) {
@@ -39,11 +54,11 @@ function createTargetGroup (tgName) {
     Port: 3000,
     Protocol: 'HTTP',
     VpcId: vpcId
-  }
+  };
 
   return new Promise((resolve, reject) => {
     elbv2.createTargetGroup(params, (err, data) => {
-      if (err) reject(err)
+      if (err) reject(err);
       else resolve(data)
     })
   })
@@ -60,11 +75,11 @@ function createListener (tgArn, lbArn) {
     LoadBalancerArn: lbArn,
     Port: 80,
     Protocol: 'HTTP'
-  }
+  };
 
   return new Promise((resolve, reject) => {
     elbv2.createListener(params, (err, data) => {
-      if (err) reject(err)
+      if (err) reject(err);
       else resolve(data)
     })
   })
