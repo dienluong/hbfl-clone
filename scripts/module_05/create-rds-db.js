f41b99d0e644f9156508ee1691696052cfccc3d7
Dien Luong <dysonlu@protonmail.com>
2020-04-21 14:38:07 -0400

    New: implemented RDS, ElastiCache and DynamoDB scripts

--- scripts/module_05/create-rds-db.js
+++ scripts/module_05/create-rds-db.js
@@ -1,22 +1,31 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
-const ec2 = new AWS.EC2()
-// TODO: Create an rds object
-const dbName = 'user'
+const ec2 = new AWS.EC2();
+const rds = new AWS.RDS();
+const dbName = 'user';
 
 createSecurityGroup(dbName)
 .then(sgId => createDatabase(dbName, sgId))
 .then(data => console.log(data))
 
 function createDatabase (dbName, sgId) {
-  // TODO: Create the params object
+  const params = {
+    AllocatedStorage: 5,
+    DBInstanceClass: 'db.t2.micro',
+    DBInstanceIdentifier: dbName,
+    Engine: 'mysql',
+    DBName: dbName,
+    VpcSecurityGroupIds: [ sgId ],
+    MasterUsername: 'admin',
+    MasterUserPassword: 'mypassword',
+  };
 
-  return new Promise((resolve, reject) => {
-    // TODO: Create the db instance
-  })
+  return rds.createDBInstance(params).promise();
+  // return new Promise((resolve, reject) => {
+  // })
 }
 
 function createSecurityGroup (dbName) {
--- scripts/module_05/populate-dynamo-table.js
+++ scripts/module_05/populate-dynamo-table.js
@@ -2,10 +2,10 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-// TODO: Declare dynamoDB DocumentClient object
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 helpers.getHamsterData()
 .then(data => populateTable('hamsters', data))
@@ -14,9 +14,20 @@ helpers.getHamsterData()
 .then(data => console.log(data))
 
 function populateTable (tableName, data) {
-  // TODO: Create params const object
+  const params = {
+    RequestItems: {
+      [tableName]: data.map(item => ({
+        PutRequest: {
+          Item: item,
+        },
+      }))
+    },
+  };
 
   return new Promise((resolve, reject) => {
-    // TODO: Call batch write function
-  })
+      docClient.batchWrite(params, (err, data) => {
+        if (err) { reject(err); }
+        else { resolve(data); }
+      });
+  });
 }
