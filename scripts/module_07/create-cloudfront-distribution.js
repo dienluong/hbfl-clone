9563c5176b95fa9ff6bd1b06901fd02b9dd62c6c
Dien Luong <dysonlu@gmail.com>
2020-04-23 09:38:53 -0400

    New: implemented cloudfront scripts

--- scripts/module_07/create-cloudfront-distribution.js
+++ scripts/module_07/create-cloudfront-distribution.js
@@ -1,19 +1,28 @@
 // Imports
-const AWS = require('aws-sdk')
-const cfParams = require('./cloudfront-parameters')
+const AWS = require('aws-sdk');
+const cfParams = require('./cloudfront-parameters');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-// TODO: Create CloudFront SDK Object
+const cf = new AWS.CloudFront();
 
-createDistribution('/* TODO: Add your bucket name */')
-.then(data => console.log(data))
+createDistribution('hamster-bucket-dysonlu')
+.then(data => console.log(data));
 
 function createDistribution (bucketName) {
-  // TODO: Create params const object
+  const params = {
+    DistributionConfig: {
+      CallerReference: `${Date.now()}`,
+      Comment: 'HBFL CloudFront Distribution',
+      DefaultCacheBehavior: cfParams.defaultCacheBehavior(bucketName),
+      Origins: cfParams.origins(bucketName),
+      HttpVersion: 'http2',
+      PriceClass: 'PriceClass_100',
+      IsIPV6Enabled: true,
+      Enabled: true,
+    },
+  };
 
-  return new Promise((resolve, reject) => {
-    // TODO: Call createDistribution
-  })
+  return cf.createDistribution(params).promise();
 }
--- scripts/module_08/create-sqs-queue.js
+++ scripts/module_08/create-sqs-queue.js
@@ -1,18 +1,25 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-// TODO: Create sqs object
-const queueName = 'hamster-race-results'
+const sqs = new AWS.SQS();
+const queueName = 'hamster-race-results';
 
 createQueue(queueName)
 .then(data => console.log(data))
 
 function createQueue (queueName) {
-  // TODO: Create params const for creating queue
-
+  const params = {
+    QueueName: queueName,
+    Attributes: {
+      DelaySeconds: '0',
+      MessageRetentionPeriod: '345600',
+      VisibilityTimeout: '30'
+      ReceiveMessageWaitTimeSeconds: '0',
+    }
+  };
   return new Promise((resolve, reject) => {
     sqs.createQueue(params, (err, data) => {
       if (err) reject(err)
